1b2504c7791a0ac72c5433ea6a10ef693bc847c4
Michael Wallner <m.wallner@neonblack.at>
2025-03-10 20:09:47 +0100

    👔 add methods for encoding/decoding multiple hashids and caching factory instance

--- src/Hashidable.php
+++ src/Hashidable.php
@@ -2,40 +2,77 @@
 
 namespace CodersCantina\Hashids;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 trait Hashidable
 {
+    /**
+     * Store hashids factory instances to prevent redundant instantiation
+     *
+     * @var array<string, \Hashids\Hashids>
+     */
+    protected static array $hashidsFactory = [];
+
+    /**
+     * Get the Hashids factory instance for this model
+     *
+     * @return \Hashids\Hashids
+     */
     public static function getHashidsFactory(): \Hashids\Hashids
     {
-        $connections = config('hashids.connections');
-        $config = Arr::get($connections, __CLASS__, $connections[config('hashids.default')]);
+        $class = static::class;
 
-        return app('hashids.factory')->make(
-            array_replace(
-                $config,
-                [
-                    'salt' => __CLASS__ . $config['salt'],
-                ]
-            )
-        );
+        if (!isset(static::$hashidsFactory[$class])) {
+            $connections = config('hashids.connections');
+            $config = Arr::get($connections, $class, $connections[config('hashids.default')]);
+
+            static::$hashidsFactory[$class] = app('hashids.factory')->make(
+                array_replace(
+                    $config,
+                    [
+                        'salt' => $class . $config['salt'],
+                    ]
+                )
+            );
+        }
+
+        return static::$hashidsFactory[$class];
     }
 
+    /**
+     * Get the route key for the model
+     *
+     * @return string
+     */
     public function getRouteKey(): string
     {
-        return self::encodeHashId(parent::getRouteKey());
+        return static::encodeHashId((int)parent::getRouteKey());
     }
 
-    public function resolveRouteBinding($hashId, $field = null)
+    /**
+     * Resolve route binding for the model
+     *
+     * @param mixed $hashId
+     * @param string|null $field
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function resolveRouteBinding($hashId, $field = null): ?Model
     {
-        return self::findByHashIdOrFail($hashId);
+        return static::findByHashIdOrFail($hashId);
     }
 
+    /**
+     * Find a model by its hashid
+     *
+     * @param string|null $hashId
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public static function findByHashId(?string $hashId = null): ?Model
     {
-        if ($id = self::decodeHashId($hashId)) {
-            $model = new self();
+        if ($id = static::decodeHashId($hashId)) {
+            $model = new static();
 
             return $model->where($model->getRouteKeyName(), $id)->first();
         }
@@ -43,25 +80,102 @@ trait Hashidable
         return null;
     }
 
-    public static function findByHashIdOrFail(?string $hashId, $columns = ['*']): ?Model
+    /**
+     * Find a model by its hashid or fail
+     *
+     * @param string|null $hashId
+     * @param array $columns
+     * @return \Illuminate\Database\Eloquent\Model
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    public static function findByHashIdOrFail(?string $hashId, $columns = ['*']): Model
     {
-        $model = new self();
+        $id = static::decodeHashId($hashId);
+        $model = new static();
 
-        return $model->findOrFail(self::decodeHashId($hashId), $columns);
+        return $model->findOrFail($id, $columns);
     }
 
+    /**
+     * Decode a hashid into an integer ID
+     *
+     * @param string|null $value
+     * @return int|null
+     */
     public static function decodeHashId(?string $value): ?int
     {
         if (!$value) {
             return null;
         }
-        $result = self::getHashidsFactory()->decode($value);
+        $result = static::getHashidsFactory()->decode($value);
 
         return count($result) ? $result[0] : null;
     }
 
+    /**
+     * Encode an integer ID into a hashid
+     *
+     * @param int $value
+     * @return string
+     */
     public static function encodeHashId(int $value): string
     {
-        return self::getHashidsFactory()->encode($value);
+        return static::getHashidsFactory()->encode($value);
+    }
+
+    /**
+     * Encode an array of integer IDs into hashids
+     *
+     * @param array<int> $ids
+     * @return array<string>
+     */
+    public static function encodeHashIds(array $ids): array
+    {
+        return array_map(function (int $id): string {
+            return static::encodeHashId($id);
+        }, $ids);
+    }
+
+    /**
+     * Decode an array of hashids into integer IDs
+     *
+     * @param array<string> $hashIds
+     * @return array<int>
+     */
+    public static function decodeHashIds(array $hashIds): array
+    {
+        if (empty($hashIds)) {
+            return [];
+        }
+
+        $result = [];
+        foreach ($hashIds as $hashId) {
+            if ($id = static::decodeHashId($hashId)) {
+                $result[] = $id;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Find multiple models by their hashids
+     *
+     * @param array<string> $hashIds
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public static function findByHashIds(array $hashIds): Collection
+    {
+        if (empty($hashIds)) {
+            return Collection::make();
+        }
+
+        $ids = static::decodeHashIds($hashIds);
+
+        if (empty($ids)) {
+            return Collection::make();
+        }
+
+        return static::whereIn((new static)->getRouteKeyName(), $ids)->get();
     }
 }
--- tests/HashidableTest.php
+++ tests/HashidableTest.php
@@ -5,6 +5,7 @@ namespace CodersCantina\Hashids;
 use GrahamCampbell\TestBench\AbstractPackageTestCase;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\QueryException;
+use Ramsey\Collection\Collection;
 
 class HashidableTest extends AbstractPackageTestCase
 {
@@ -66,9 +67,64 @@ class HashidableTest extends AbstractPackageTestCase
         $result = Foo::findByHashId(null);
         $this->assertNull($result);
     }
+
+    /** @test */
+    public function itEncodesMultipleIds()
+    {
+        $result = Foo::encodeHashIds([1, 2, 3]);
+        $this->assertEquals(['Za', 'wy', 'Oo'], $result);
+    }
+
+    /** @test */
+    public function itDecodesMultipleHashids()
+    {
+        $result = Foo::decodeHashIds(['Za', 'wy', 'Oo']);
+        $this->assertEquals([1, 2, 3], $result);
+    }
+
+    /** @test */
+    public function itHandlesEmptyArraysWhenDecoding()
+    {
+        $result = Foo::decodeHashIds([]);
+        $this->assertEquals([], $result);
+    }
+
+    /** @test */
+    public function itHandlesInvalidHashidsWhenDecoding()
+    {
+        $result = Foo::decodeHashIds(['invalid', 'Za', 'also-invalid']);
+        $this->assertEquals([1], $result);
+    }
+
+    /** @test */
+    public function itBuildsFindByHashIdsQuery()
+    {
+        try {
+            $result = Foo::findByHashIds(['Za', 'wy']);
+        } catch (QueryException $exception) {
+            $this->assertEquals('select * from "foos" where "id" in (?, ?)', $exception->getSql());
+            $this->assertEquals([1, 2], $exception->getBindings());
+        }
+    }
+
+    /** @test */
+    public function itCachesHashidsFactory()
+    {
+        // Call twice to verify caching works
+        $factory1 = Foo::getHashidsFactory();
+        $factory2 = Foo::getHashidsFactory();
+
+        // Should be the same instance
+        $this->assertSame($factory1, $factory2);
+    }
 }
 
 class Foo extends Model
+{
+    use Hashidable;
+}
+
+class Bar extends Model
 {
     use Hashidable;
 }
